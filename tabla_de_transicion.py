e6f2bdbc87eb6f774d93352f061e75e0b6fa069e
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-08 23:27:51 -0500

    Creando tabla de transiciones a traves de excel

---
+++ tabla_de_transicion.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+class Tabla():
+    def __init__(self, archivo = '', tabla = ''):
+        self._archivo = archivo
+        self._tabla = tabla
+    
+    @property
+    def archivo(self):
+        return self._archivo
+    @archivo.setter
+    def ruta(self, archivo):
+        self._archivo = archivo
+        
+    @property
+    def tabla(self):
+        return self._tabla
+    @tabla.setter
+    def tabla(self, tabla):
+        self._tabla = tabla
+        
+    #Abrimos la hoja de excel
+    def cargarTabla(self):
+        self._tabla = pd.read_excel(self._archivo)
+        print(self._tabla.head())
+    
+
+excel = Tabla("Tabla-transiciones.xlsx")
+excel.cargarTabla()
+
+
